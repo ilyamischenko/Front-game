b7ee73545a118aa882763e448c23bb83e898bc51
Ilya Mishencko <ilyamishencko@gmail.com>
2024-02-11 16:27:21 +0300

    first commit

---
+++ game/main-game.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeRect(30, 30, 1700, 800);
+
+ctx.fillStyle = "green";
+
+ctx.fillRect(50, 50, 1600, 750);
+
+ctx.strokeRect(500, 550, 800, 200);
+
+ctx.strokeRect(500, 100, 800, 200);
+
+ctx.strokeRect(100, 300, 200, 300);
