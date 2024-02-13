4ddaa4e0cc68008553523bba3a0b86c5a5903c8e
Ilya Mishencko <ilyamishencko@gmail.com>
2024-02-13 12:52:31 +0300

    regesrt

---
+++ regestration/main.js
@@ -0,0 +1,22 @@
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addPost();
+});
+
+function addPost() {
+  const form = document.querySelector("form");
+
+  let data = {
+    name: form.querySelector("[name='name']").value,
+    email: form.querySelector("[name='email']").value,
+    password: form.querySelector("[name= 'password']").value,
+  };
+
+  fetch("	https://jsonplaceholder.org/users", {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then(console.log);
+}
