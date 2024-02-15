f657ae6c3d634d5851d30e0c279fbddd559bd20e
Ilya Mishencko <ilyamishencko@gmail.com>
2024-02-15 11:30:38 +0300

    user-sign/js

---
+++ user-sign/main-sing.js
@@ -0,0 +1,21 @@
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addPost();
+});
+
+function addPost() {
+  const form = document.querySelector("form");
+
+  let data = {
+    email: form.querySelector("[name='email']").value,
+    password: form.querySelector("[name= 'password']").value,
+  };
+
+  fetch("	https://jsonplaceholder.org/users", {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then(console.log);
+}
